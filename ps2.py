21fd5e7c5e09fd16dd87b28efe830bf9cc3944e2
Aghil Sabu <aghilsabu@gmail.com>
2019-09-18 11:55:03 +0530

    ps2 all cases added and added comments

--- ps2.py
+++ ps2.py
@@ -47,6 +47,9 @@ def bdisplay(board):
 board=[]
 for i in range(size*size):
     board.append(-1)
+numbers=[]
+for i in range(size*size):
+    numbers.append(False)
 # bdisplay(board)
 
 # function to check whether game is over
@@ -67,37 +70,71 @@ def gameover(board,lines):
     return False
 # Starting the Game
 player=0
-while(not gameover(board,lines)):
-    system("clear")
-    print("\n\t Welcome to the Game!\n")
-    bdisplay(board)
-    print("\n\n\n\tplayer",player+1,"'s chance")
-    pos=input("Enter Position : ")
-    val=input("Enter value : ")
-    if(pos.isnumeric() and val.isnumeric()):
-        pos=int(pos)
-        val=int(val)
-    else:
-        print("\n\tInvalid Input\n\nPress ENTER to continue")
-        input()
-        continue
-    if(pos<1 or val<1 or pos>size*size or val>size*size or board[pos-1]!=-1):
-        print("\n\tInvalid Input\n\nPress ENTER to continue")
-        input()
-        continue
-    elif(player==0 and val%2==0):
-        print("\nERROR : Player 1 is only allowed to enter odd values")
-        input()
-        continue
-    elif(player==1 and val%2!=0):
-        print("\nERROR : Player 2 is only allowed to enter even values")
-        input()
-        continue
-    else:
-        board[pos-1]=val
-        if(gameover(board,lines)):
-            print("\tPlayer",player+1,"wins the game")
+while(1):
+    # checking if a game is already over, then will show option to exit/continue
+    if(player==size*size):
+        system("clear")
+        new=input("Do you want to start a new Game (y/n) : ")
+        if(new=="y"):
+            for i in range(size*size):
+                board[i]=-1
+                numbers[i]=False
+            player=0
+        elif(new=="n"):
             break
         else:
-            player=player^1
+            continue
+    # displaying game menus
+    while(1):
+        system("clear")
+        print("\n\t Welcome to the Game!\n")
+        # Displaying the board
+        bdisplay(board)
+        print("\n\n\n\tplayer",(player%2)+1,"'s chance")
+        pos=input("Enter Position : ")
+        val=input("Enter value : ")
+        # checking whether inputs are valid
+        if(pos.isnumeric() and val.isnumeric()):
+            pos=int(pos)
+            val=int(val)
+        else:
+            print("\n\tInvalid Input\n\nPress ENTER to continue")
+            input()
+            continue
+        if(pos<1 or val<1 or pos>size*size or val>size*size or board[pos-1]!=-1 or numbers[val-1]):
+            print("\n\tInvalid Input\n\nPress ENTER to continue")
+            input()
+            continue
+        elif((player%2)==0 and val%2==0):
+            print("\nERROR : Player 1 is only allowed to enter odd values")
+            print("\nPress ENTER to continue")
+            input()
+            continue
+        elif((player%2)==1 and val%2!=0):
+            print("\nERROR : Player 2 is only allowed to enter even values")
+            print("\nPress ENTER to continue")
+            input()
+            continue
+        else:
+            # updating the board with entered value
+            board[pos-1]=val
+            # checking if player has won the game
+            if(gameover(board,lines)):
+                print("\tPlayer",(player%2)+1,"wins the game")
+                print("\nPress ENTER to continue")
+                player=size*size
+                input()
+                break
+            # checking if the board is completely filled
+            elif(player==size*size-1):
+                print("\tGame ends in a draw")
+                print("\nPress ENTER to continue")
+                input()
+                player=size*size
+                break
+            # changing player
+            else:
+                numbers[val-1]=True
+                player=player+1
+    
 
